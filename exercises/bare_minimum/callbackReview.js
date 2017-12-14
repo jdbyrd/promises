7944d01b3e2c014c19f5e4050cc61eaf646b9f5c
jdbyrd <jd3yrd@gmail.com>
2017-12-14 12:31:29 -0600

    In progress

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,24 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, 'utf8', function(err, data) {
+    if (err) { callback(err, data); }
+    let dataList = data.split('\n');
+    callback(err, dataList[0]);
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request(url, (err, res) => {
+    if (err) {
+      callback(err); 
+    } else {
+      callback(err, res.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -11,11 +11,29 @@ var Promise = require('bluebird');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  return new Promise ((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', function(err, data) {
+      if (err) { 
+        reject(err); 
+      } else { 
+        const dataList = data.split('\n');
+        resolve(dataList[0]);
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise ((resolve, reject) => {
+    request(url, (err, res) => {
+      if (err) {
+        reject(err); 
+      } else {
+        resolve(res.statusCode);
+      }
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
